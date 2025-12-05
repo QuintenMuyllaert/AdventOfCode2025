c5fb2881b7ea5f8d45acd79ad231cdb48ff436c1
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2025-12-05 17:20:35 +0100

    ✨ Day 5 pt2

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -2,7 +2,7 @@
 
 import { describe, expect, test } from "bun:test";
 
-class SpoilerAlert {
+class SpoilerAlertP1 {
 	fresh = 0;
 
 	ranges: number[][] = [];
@@ -36,17 +36,95 @@ class SpoilerAlert {
 	}
 }
 
+class SpoilerAlertP2 {
+	fresh = 0;
+
+	ranges: number[][] = [];
+
+	constructor(input: string) {
+		const lines = input.trim().split("\n");
+
+		let smallest = +Infinity;
+		let biggest = -Infinity;
+
+		const addingFnRanges = (line: string) => {
+			const [min, max] = line.split("-").map(Number) as [number, number];
+			if (min < smallest) smallest = min;
+			if (max > biggest) biggest = max;
+			this.ranges.push([min, max, max - min + 1]);
+		};
+
+		for (const line of lines) {
+			if (line.trim() === "") break;
+
+			addingFnRanges(line);
+		}
+
+		this.ranges.sort((a, b) => a[0]! - b[0]!);
+
+		const newRanges: [number, number, number][] = [];
+		let [currentMin, currentMax] = this.ranges[0]! as [number, number];
+
+		for (const range of this.ranges!) {
+			const [min, max] = range! as [number, number];
+			if (min <= currentMax) {
+				currentMax = Math.max(currentMax, max);
+			} else {
+				newRanges.push([currentMin, currentMax, currentMax - currentMin + 1]);
+				currentMin = min;
+				currentMax = max;
+			}
+		}
+
+		newRanges.push([currentMin, currentMax, currentMax - currentMin + 1]);
+
+		for (let i = 0; i < newRanges.length; i++) {
+			const [_curMin, _curMax, curDist] = newRanges[i]! as [number, number, number];
+
+			this.fresh += curDist;
+		}
+	}
+}
+
 describe("Part 1", () => {
 	test("Example Ingredients Database", async () => {
 		const { default: example } = await import("./example.txt");
-		const spoilers = new SpoilerAlert(example);
+		const spoilers = new SpoilerAlertP1(example);
 
 		expect(spoilers.fresh).toEqual(3);
 	});
-	test("Example Ingredients Database", async () => {
+	test("Ingredients Database", async () => {
 		const { default: input } = await import("./input.txt");
-		const spoilers = new SpoilerAlert(input);
+		const spoilers = new SpoilerAlertP1(input);
 
 		console.log(`There are ${spoilers.fresh} fresh ingredients.`);
+	});
+});
+
+describe("Part 2", () => {
+	test("Example Ingredients Database", async () => {
+		const { default: example } = await import("./example.txt");
+		const spoilers = new SpoilerAlertP2(example);
+
+		expect(spoilers.fresh).toEqual(14);
+	});
+	test("Example Ingredients Database", async () => {
+		const { default: example } = await import("./example2.txt");
+		const spoilers = new SpoilerAlertP2(example);
+
+		expect(spoilers.fresh).toEqual(21);
+	});
+	test("Progz Input - Because I'm doubting my own input.txt", async () => {
+		const { default: input } = await import("./input-progz.txt");
+		const spoilers = new SpoilerAlertP2(input);
+
+		console.log(`There are ${spoilers.fresh} fresh ingredient ID's.`);
+		expect(spoilers.fresh).toEqual(350684792662845);
+	});
+	test("Ingredients Database", async () => {
+		const { default: input } = await import("./input.txt");
+		const spoilers = new SpoilerAlertP2(input);
+
+		console.log(`There are ${spoilers.fresh} fresh ingredient ID's.`);
 	});
 });
